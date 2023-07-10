7b7252f9ab3254ba94ca10b9529d44c13b31558d
KevinLeleux <beholderer@gmail.com>
2023-07-10 18:10:41 +0200

    :rocket: feat: resultats route added

--- src/router.tsx
+++ src/router.tsx
@@ -9,16 +9,20 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import MentionsLegales from './components/MentionsLegales/MentionsLegales';
 import Orientation from './components/Orientation/Orientation';
+import Resultats from './components/Resultats/Resultats';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-      <Route path="/orientation" element={<Orientation />} />
-      <Route path="/admin" element={<Login />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/mentions-legales" element={<MentionsLegales />} />
-    </Route>
+    <>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="/orientation" element={<Orientation />} />
+        <Route path="/admin" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/mentions-legales" element={<MentionsLegales />} />
+      </Route>
+      <Route path="/resultats" element={<Resultats />} />
+    </>
   )
 );
 
