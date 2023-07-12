9e50f13f7fe15dfc22f9b9f6835e408504151253
KevinLeleux <beholderer@gmail.com>
2023-07-12 13:47:09 +0200

    :rocket: feat: map on toulouse, geolocation tests

--- src/components/Resultats/Map/Map.tsx
+++ src/components/Resultats/Map/Map.tsx
@@ -1,19 +1,47 @@
+import { useEffect, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-
 import './Map.scss';
+import LocationMarker from './Marker/Marker';
+
+interface Position {
+  coords: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
+interface ErrorMessage {
+  message: string;
+}
 
 function Map() {
+  const [location, setLocation] = useState<number[]>([]);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setError('Geolocation is not supported by this browser.');
+      return;
+    }
+
+    function handleSuccess(position: Position) {
+      const { latitude, longitude } = position.coords;
+      setLocation([latitude, longitude]);
+    }
+
+    function handleError(errorMessage: ErrorMessage) {
+      setError(errorMessage.message);
+    }
+
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+  }, []);
+
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[43.6, 1.4333]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
     </MapContainer>
   );
 }
