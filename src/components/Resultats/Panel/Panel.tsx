dd0e3c316bc25e8ffbbbb4d12318036a68bf9acd
KevinLeleux <beholderer@gmail.com>
2023-07-13 09:55:08 +0200

    :rocket: feat: card impelemented

--- src/components/Resultats/Panel/Panel.tsx
+++ src/components/Resultats/Panel/Panel.tsx
@@ -1,7 +1,17 @@
+import Card from '../Card/Card';
 import './Panel.scss';
 
 function Panel() {
-  return <section className="panel">Panel</section>;
+  return (
+    <section className="panel">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </section>
+  );
 }
 
 export default Panel;
