9909983b06448c66fd3a310e53f33de5c045bee7
KevinLeleux <beholderer@gmail.com>
2023-07-14 00:52:01 +0200

    :beetle: fix: react-query module removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouterProvider } from 'react-router-dom';
 
 import router from './router';
 import './styles/index.scss';
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
