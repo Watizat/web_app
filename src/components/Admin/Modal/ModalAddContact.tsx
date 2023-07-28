e4b86e69fd4fa0b171bcff4ae8fdb11c32e8adf0
KevinLeleux <beholderer@gmail.com>
2023-07-29 01:59:09 +0200

    :rocket: feat: form validation on add contact modal

--- src/components/Admin/Modal/ModalAddContact.tsx
+++ src/components/Admin/Modal/ModalAddContact.tsx
@@ -1,20 +1,36 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
 import { setAdminOrganism } from '../../../store/reducers/admin';
 import { axiosInstance } from '../../../utils/axios';
+
 import './Modal.scss';
 
 interface ModalProps {
   setIsActive: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+interface Inputs {
+  name: string;
+  comment: string;
+  mail: string;
+  job: string;
+  phone: number;
+  role: boolean;
+  actualisation: boolean;
+  organisme: number;
+}
+
 function ModalAddContact({ setIsActive }: ModalProps) {
   const dispatch = useAppDispatch();
   const id = useAppSelector((state) => state.admin.organism?.id);
 
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const form = new FormData(event.currentTarget);
-    const data = Object.fromEntries(form);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
     try {
       const response = await axiosInstance.post('/items/contact', {
         ...data,
@@ -27,30 +43,57 @@ function ModalAddContact({ setIsActive }: ModalProps) {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
+  const validateEmail = (value: string) => {
+    // Si la valeur est vide, la validation réussit
+    if (!value) {
+      return true;
+    }
+    // Expression régulière pour valider un email
+    const emailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return emailPattern.test(value) || `Cette adresse email n'est pas valide`;
+  };
 
   return (
     <div className="modal">
       <div className="modal-main">
         <h1 className="modal-title">Ajouter un contact</h1>
-        <form className="modal-list" onSubmit={handleSubmit}>
-          <input type="number" defaultValue={id} hidden name="organisme" />
+        <form className="modal-list" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="number"
+            defaultValue={id}
+            hidden
+            {...register('organisme')}
+          />
           <div className="modal-case">
             <h4 className="modal-case__title">Nom du contact</h4>
-            <input className="modal-case__inputTxt" type="text" name="name" />
+            <input
+              className={
+                errors.name
+                  ? 'modal-case__inputTxt error'
+                  : 'modal-case__inputTxt'
+              }
+              type="text"
+              {...register('name', { required: 'Ce champs est requis' })}
+            />
+            {errors.name && <small>{errors.name.message}</small>}
           </div>
           <div className="modal-case">
             <h4 className="modal-case__title">Commentaire </h4>
             <input
               className="modal-case__inputTxt"
               type="text"
-              name="comment"
-              placeholder="Informations concernant le contact"
+              {...register('comment')}
             />
           </div>
           <div className="modal-case">
             <h4 className="modal-case__title">Fonction</h4>
-            <input className="modal-case__inputTxt" type="text" name="job" />
+            <input
+              className="modal-case__inputTxt"
+              type="text"
+              {...register('job')}
+            />
           </div>
           <div className="modal-contact__modes">
             <div className="modal-case">
@@ -58,16 +101,31 @@ function ModalAddContact({ setIsActive }: ModalProps) {
               <input
                 className="modal-case__inputTxt modal-contact__mail"
                 type="text"
-                name="mail"
+                {...register('mail', {
+                  validate: validateEmail,
+                })}
               />
+              {errors.mail?.message && <small>{errors.mail.message}</small>}
             </div>
             <div className="modal-case">
               <h4 className="modal-case__title">Telephone</h4>
               <input
                 className="modal-case__inputTxt"
                 type="number"
-                name="phone"
+                {...register('phone', {
+                  minLength: {
+                    value: 10,
+                    message:
+                      'Le numéro de téléphone doit comporter au moins 10 chiffres.',
+                  },
+                  maxLength: {
+                    value: 10,
+                    message:
+                      'Le numéro de téléphone ne peut pas comporter plus de 10 chiffres.',
+                  },
+                })}
               />
+              {errors.phone?.message && <small>{errors.phone.message}</small>}
             </div>
           </div>
           <div className="modal-case">
@@ -75,14 +133,14 @@ function ModalAddContact({ setIsActive }: ModalProps) {
             <div className=" modal-contact__roles">
               <label className="modal-contact__private">
                 Publicité du contact
-                <select name="role">
+                <select {...register('role', { required: true })}>
                   <option value="false">Privé</option>
                   <option value="true">Public</option>
                 </select>
               </label>
               <label className="modal-contact__actu">
                 Contact pour actualisation
-                <select name="actualisation">
+                <select {...register('actualisation', { required: true })}>
                   <option value="false">Non</option>
                   <option value="true">Oui</option>
                 </select>
