2ae2c0b9b17efa80b54c0d478725fbe6f6a8ca6e
Laura <123942872+LauraBrizard@users.noreply.github.com>
2023-07-28 13:59:02 +0200

    Navitia
    Essai multi requêtes API imbriqués del muerte

--- src/components/Organisme/Infos/Transport/Transport.tsx
+++ src/components/Organisme/Infos/Transport/Transport.tsx
@@ -3,35 +3,96 @@ import { useAppSelector } from '../../../../hooks/redux';
 import './Transport.scss';
 import navitiaInstance from '../../../../utils/navitia';
 import { Organism } from '../../../../@types/organism';
+import classNames from 'classnames';
 
-interface API {
+interface Endpoint {
   lines: {
     id: string;
     code: string;
     name: string;
     color: string;
     text_color: string;
+    commercial_mode: { name: string };
   }[];
 }
 
+interface Place {
+  lines: {
+    id: string;
+    code: string;
+    name: string;
+    color: string;
+    text_color: string;
+    commercial_mode: { name: string };
+  }[];
+}
+
+interface StopAreas {
+  stop_areas: [id: string];
+}
+
 function Transport() {
-  const [data, setData] = useState<API | null>(null);
+  const [data, setData] = useState([]);
   const organism = useAppSelector(
     (state) => state.organism.organism as Organism
   );
 
   useEffect(() => {
-    const endpoint = `/coverage/fr-sw/coord/${organism.longitude}%3B${organism.latitude}/lines`;
+    const fetchData = async () => {
+      try {
+        const endpoint = `/coverage/fr-sw/coord/${organism.longitude}%3B${organism.latitude}/lines`;
+        const response = await navitiaInstance.get(endpoint);
+        const endPoint = response.data as Endpoint;
+        const promises = endPoint.lines.map(async (line) => {
+          const stopAreas = `coverage/fr-sw/lines/${line.id}/stop_areas`;
+          try {
+            const stopAreasResponse = await navitiaInstance.get(stopAreas);
+            const stopAreasData = stopAreasResponse.data as StopAreas;
+            console.log('stoparea :', stopAreasData);
+            const stopsPromises = stopAreasData.stop_areas.map(async (stop) => {
+              // console.log('stop dans le map :', stop);
+              const placeNearby = `coverage/fr-sw/stop_areas/${stop.id}/places_nearby`;
+              // console.log('appel api placenearby :', placeNearby);
+              try {
+                const placeNearbyResponse = await navitiaInstance.get(
+                  placeNearby
+                );
+                // console.log('placenearby dans le map :', placeNearbyResponse);
+                return placeNearbyResponse.data;
+              } catch (error) {
+                console.error(
+                  "Erreur lors de l'appel API des placesNearby",
+                  error
+                );
+                return null;
+              }
+            });
 
-    navitiaInstance
-      .get(endpoint)
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
+            console.log('stoppromises :', stopsPromises);
+            const responses = await Promise.all(stopsPromises);
+            console.log('les réponses :', responses);
+            const filteredResponses = responses.filter((data) => data !== null);
+            console.log('les réponses filtrer :', filteredResponses);
+            const formattedData = [];
+            filteredResponses.forEach((response, index) => {
+              const busLine = endPoint.lines[index].name;
+              const stopName = stopAreasData[index].name;
+              const { distance } = response;
+              formattedData.push({ busLine, stopName, distance });
+            });
+            setData(formattedData);
+            console.log(formattedData);
+          } catch (error) {
+            console.error("Erreur lors de l'appel API des stopAreas", error);
+          }
+        });
+        await Promise.all(promises);
+      } catch (error) {
         console.error("Erreur lors de l'appel API:", error);
-      });
-  }, [setData, organism]);
+      }
+    };
+    fetchData();
+  }, [organism]);
 
   // si l'organism n'existe pas
   if (data === null) {
@@ -46,16 +107,23 @@ function Transport() {
   return (
     <article>
       <h3>Accès en transports</h3>
-      {data.lines.map((line) => (
-        <p key={line.id}>
-          <i className="las la-bus-alt" />
+      {data.map((item) => (
+        <p className="transport" key={item.id}>
+          <i
+            className={
+              item.busLine.commercial_mode.name === 'Bus'
+                ? classNames('las la-bus-alt')
+                : classNames('las la-subway')
+            }
+          />
           <span
+            className="transport-num"
             style={{
-              backgroundColor: `#${line.color}`,
-              color: `#${line.text_color}`,
+              backgroundColor: `#${item.busLine.color}`,
+              color: `#${item.busLine.text_color}`,
             }}
           >
-            {line.code}
+            {item.busLine.code}
           </span>
 
           {line.name}
