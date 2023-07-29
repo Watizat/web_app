598e9f1d770ce3bb3b134585bb38f75bd538cdc5
KevinLeleux <beholderer@gmail.com>
2023-07-30 00:04:04 +0200

    :rocket: feat: form validation on edit an organism  modal

--- src/components/Admin/Modal/ModalEditInfos.tsx
+++ src/components/Admin/Modal/ModalEditInfos.tsx
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Inputs } from '../../../@types/formInputs';
 import { Organism } from '../../../@types/organism';
 import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
 import { setAdminOrganism } from '../../../store/reducers/admin';
@@ -10,27 +13,39 @@ interface ModalProps {
 }
 
 function ModalEditInfos({ setIsActive, organism }: ModalProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
   const id = useAppSelector((state) => state.admin.organism?.id as number);
   const dispatch = useAppDispatch();
 
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const form = new FormData(event.currentTarget);
-    const data = Object.fromEntries(form);
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const address = `${data.address} ${data.zipcode} ${data.city}`;
 
     try {
+      const geolocResponse = await axios.get(
+        `https://api-adresse.data.gouv.fr/search/?q=${address}`
+      );
+
+      const [longitude, latitude] =
+        geolocResponse.data.features[0].geometry.coordinates;
       const response = await axiosInstance.patch(`/items/organisme/${id}`, {
         ...data,
+        latitude,
+        longitude,
         service: null,
       });
       if (response.status === 200) {
         dispatch(setAdminOrganism(id));
       }
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.log(error);
     }
     setIsActive(false);
-  }
+  };
 
   return (
     <div className="modal">
@@ -38,15 +53,16 @@ function ModalEditInfos({ setIsActive, organism }: ModalProps) {
         <h1 className="modal-title">
           Modifier les informations de l&apos;organisme
         </h1>
-        <form className="modal-list" onSubmit={handleSubmit}>
+        <form className="modal-list" onSubmit={handleSubmit(onSubmit)}>
           <div className="modal-case">
             <h4 className="modal-case__title">Organisme</h4>
             <input
               className="modal-case__inputTxt"
               type="text"
               defaultValue={organism.name}
-              name="name"
+              {...register('name', { required: 'Ce champs est requis' })}
             />
+            {errors.name && <small>{errors.name.message}</small>}
           </div>
           <div className="modal-case">
             <h4 className="modal-case__title">Adresse</h4>
@@ -54,8 +70,9 @@ function ModalEditInfos({ setIsActive, organism }: ModalProps) {
               className="modal-case__inputTxt"
               type="text"
               defaultValue={organism.address}
-              name="address"
+              {...register('address', { required: 'Ce champs est requis' })}
             />
+            {errors.address && <small>{errors.address.message}</small>}
           </div>
           <div className="modal-case">
             <h4 className="modal-case__title">Ville</h4>
@@ -63,8 +80,9 @@ function ModalEditInfos({ setIsActive, organism }: ModalProps) {
               className="modal-case__inputTxt"
               type="text"
               defaultValue={organism.city}
-              name="city"
+              {...register('city', { required: 'Ce champs est requis' })}
             />
+            {errors.city && <small>{errors.city.message}</small>}
           </div>
           <div className="modal-case">
             <h4 className="modal-case__title">Code postal</h4>
@@ -72,8 +90,9 @@ function ModalEditInfos({ setIsActive, organism }: ModalProps) {
               className="modal-case__inputTxt"
               type="number"
               defaultValue={organism.zipcode}
-              name="zipcode"
+              {...register('zipcode', { required: 'Ce champs est requis' })}
             />
+            {errors.zipcode && <small>{errors.zipcode.message}</small>}
           </div>
           <div className="modal-case">
             <h4 className="modal-case__title">Site web</h4>
@@ -81,7 +100,7 @@ function ModalEditInfos({ setIsActive, organism }: ModalProps) {
               className="modal-case__inputTxt"
               type="text"
               defaultValue={organism.website}
-              name="website"
+              {...register('website')}
             />
           </div>
           <div className="modal-actions">
