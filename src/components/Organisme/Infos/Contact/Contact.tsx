0bba9cba16f0c3241f4ab14ef3aaa0e4c782ccdc
Alice OsDel <github.underpay178@slmail.me>
2023-08-02 17:08:57 +0200

    Fiche organisme Style sur affichage contacts en responsive

--- src/components/Organisme/Infos/Contact/Contact.tsx
+++ src/components/Organisme/Infos/Contact/Contact.tsx
@@ -9,37 +9,42 @@ function Contact() {
     return <span>Erreur</span>;
   }
   return (
-    <article>
+    <article className="organisme-infos--articlecontact">
       <h3>Contact</h3>
-      <div className="organisme-infos--maincontact">
-        <p>
-          {organism.address} - <span>{organism.zipcode}</span>{' '}
-          <span>{organism.city}</span>
-        </p>
-        <Link to="tel:+3300" className="organisme-infos--link">
-          <p>{organism.phone}</p>
-        </Link>
-        <Link to="tel:+3300" className="organisme-infos--link">
-          <p>{organism.mail}</p>
-        </Link>
+      <div className="organisme-infos--allcontact">
+        <div className="organisme-infos--maincontact">
+          <p>
+            {organism.address} - <span>{organism.zipcode}</span>{' '}
+            <span>{organism.city}</span>
+          </p>
+          <Link to="tel:+3300" className="organisme-infos--link">
+            <p>{organism.phone}</p>
+          </Link>
+          <Link to="tel:+3300" className="organisme-infos--link">
+            <p>{organism.mail}</p>
+          </Link>
+        </div>
+        {organism.contacts.length > 0 &&
+          organism.contacts.map((contact) => (
+            <div className="organisme-infos--othercontact" key={contact.name}>
+              <p className="organisme-infos--othercontact-job">{contact.job}</p>
+              <p>{contact.name}</p>
+              <p>{contact.comment}</p>
+              <Link
+                to={`tel:${contact.phone}`}
+                className="organisme-infos--link"
+              >
+                <p>{contact.phone}</p>
+              </Link>
+              <Link
+                to={`mailto:${contact.mail}`}
+                className="organisme-infos--link"
+              >
+                <p>{contact.mail}</p>
+              </Link>
+            </div>
+          ))}
       </div>
-      {organism.contacts.length > 0 &&
-        organism.contacts.map((contact) => (
-          <div className="organisme-infos--othercontact" key={contact.name}>
-            <p className="organisme-infos--othercontact-job">{contact.job}</p>
-            <p>{contact.name}</p>
-            <p>{contact.comment}</p>
-            <Link to={`tel:${contact.phone}`} className="organisme-infos--link">
-              <p>{contact.phone}</p>
-            </Link>
-            <Link
-              to={`mailto:${contact.mail}`}
-              className="organisme-infos--link"
-            >
-              <p>{contact.mail}</p>
-            </Link>
-          </div>
-        ))}
     </article>
   );
 }
