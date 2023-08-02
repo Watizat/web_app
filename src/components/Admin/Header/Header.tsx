344178a13ab477494c2fc9b01458891a1105a184
KevinLeleux <beholderer@gmail.com>
2023-08-02 09:30:22 +0200

    :beetle: fix: add organism modal fixed

--- src/components/Admin/Header/Header.tsx
+++ src/components/Admin/Header/Header.tsx
@@ -18,46 +18,47 @@ function Header() {
   const dispatch = useAppDispatch();
 
   return (
-    <header id="headerAdmin">
+    <>
+      <header id="headerAdmin">
+        {/* {!isWidescreen && isOpen === false} */}
+
+        {!isOpen ? (
+          <div className="headerAdmin-hamburger">
+            <Hamburger
+              size={27}
+              toggled={isOpen}
+              toggle={() => dispatch(toggleHamburger(!isOpen))}
+            />
+          </div>
+        ) : (
+          <div className="headerAdmin-hamburger headerAdmin-hamburger__open">
+            <Hamburger
+              size={27}
+              toggled={isOpen}
+              toggle={() => dispatch(toggleHamburger(!isOpen))}
+            />
+          </div>
+        )}
+
+        <h2>
+          {pathname === '/admin/edition' && 'Edition des données'}
+          {pathname === '/admin/users' && 'Gestion des utilisateur·ices'}
+        </h2>
+
+        {pathname === '/admin/edition' && (
+          <button
+            type="button"
+            className="btn btn-sucess btn-rounded btn-flat"
+            onClick={() => setIsActiveOrganism(true)}
+          >
+            Ajouter un organisme
+          </button>
+        )}
+      </header>
       {isActiveOrganism && (
         <ModalAddOrganism setIsActive={setIsActiveOrganism} />
       )}
-
-      {/* {!isWidescreen && isOpen === false} */}
-
-      {!isOpen ? (
-        <div className="headerAdmin-hamburger">
-          <Hamburger
-            size={27}
-            toggled={isOpen}
-            toggle={() => dispatch(toggleHamburger(!isOpen))}
-          />
-        </div>
-      ) : (
-        <div className="headerAdmin-hamburger headerAdmin-hamburger__open">
-          <Hamburger
-            size={27}
-            toggled={isOpen}
-            toggle={() => dispatch(toggleHamburger(!isOpen))}
-          />
-        </div>
-      )}
-
-      <h2>
-        {pathname === '/admin/edition' && 'Edition des données'}
-        {pathname === '/admin/users' && 'Gestion des utilisateur·ices'}
-      </h2>
-
-      {pathname === '/admin/edition' && (
-        <button
-          type="button"
-          className="btn btn-sucess btn-rounded btn-flat"
-          onClick={() => setIsActiveOrganism(true)}
-        >
-          Ajouter un organisme
-        </button>
-      )}
-    </header>
+    </>
   );
 }
 
